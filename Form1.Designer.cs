a5d6374e70095d0bec92f3ef5ef428f3578ef81c
NorinRad <norinrad.39@gmail.com>
2023-11-22 16:59:17 +0100

    Continuer avec la creation du dossier Indice

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,33 +29,279 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(450, 180);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(109, 47);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Connexion";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(81, 121);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "N° Moule";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Repère";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(81, 147);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Désignation";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(81, 173);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(197, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Désignation corrigée";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(298, 173);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(197, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Repère corrigé";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(298, 147);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(197, 124);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "N° Moule corrigé";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(298, 121);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 236);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 47);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Génération des dossiers";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(197, 202);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(71, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Indice corrigé";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(298, 199);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 202);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(36, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Indice";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(81, 199);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 14;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(113, 95);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(285, 20);
+            this.textBox9.TabIndex = 18;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 98);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(95, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Document courent";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 72);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(73, 13);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "Projet courent";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(113, 69);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(285, 20);
+            this.textBox10.TabIndex = 20;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(127, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 47);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "Déconnexion";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(415, 297);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Folders Créator Tool";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using TopSolid.Cad.Design.Automating;
+using TopSolid.Cad.Drafting.Automating;
 using TopSolid.Kernel.Automating;
 
 
@@ -23,13 +24,57 @@ namespace Folder_Creator_Tool_V3
 
         private void button1_Click(object sender, EventArgs e)
         {
-            PdmObjectId CurrentProjectPdmId;
-            string CurrentProjectName;
-            List<PdmObjectId> FolderIDs = new List<PdmObjectId>();
+            PdmObjectId CurrentProjectPdmId; //Id du projet courent
+            string CurrentProjectName; //Nom du projet courent
 
+            DocumentId CurrentDocumentId; //Id du document courent
+            string TextCurrentDocumentName; //Nom du document courent
+
+            List<PdmObjectId> FolderIds = new List<PdmObjectId>(); //Liste des dossiers contenu dans le projet courent
+            List<PdmObjectId> DocumentsIds = new List<PdmObjectId>(); //Liste des documents contenu dans le projet courent
+
+            ElementId CurrentDocumentCommentaireId; //Id du commentaire du document courent
+            string TextCurrentDocumentCommentaire; //Texte du commentaire du document courent
+            ElementId CurrentDocumentDesignationId;//Id de la designation du document courent
+            string TextCurrentDocumentDesignation;//Texte de la designation du document courent
+
+            string TextBoxProjectName; //Nom du projet affiché dans la textbox
+
+            string TextBoxDesignationValue;//Texte afficher dans la textbox designation
+            string TextBoxCommentaireValue; //Texte afficher dans la textbox Commentaire
+            string TextBoxIndiceValue;//Texte afficher dans la textbox Indice
+            string TextBoxNomMouleValue;//Texte afficher dans la textbox NomMoule
+
+            string ConstituantFolderName;
+            List<string> ConstituantFolderNames = new List<string>();
+
+            string ConstituantDocumentName;
+            List<string> ConstituantDocumentNames = new List<string>();
+
+            List<string> ListDocumentsFoldersNames = new List<string>();
+
+            string NomDossierRep; //Nom du dossier repere a creer
+
+            bool test00; //Creation bool pour tester la presence des dossiers a creer dans le projet
+            bool test01;
+            bool test03;
+
+
+            string NomDossierExistant; //Variable qui contient le nom du dossier si existant
+
+            List < PdmObjectId > IndiceFolderIds = new List<PdmObjectId >();
+
+            PdmObjectId DossierExistant; //Id du dossier existant
+
+            string IndiceFolderName;
+
+
+
+
+
+
+            bool TSConnected = TopSolidDesignHost.IsConnected;
             {
-
-                bool TSConnected = TopSolidDesignHost.IsConnected;
                 if (TSConnected == false)
                     MessageBox.Show("Tentative de connexion a TopSolid");
                 else
@@ -49,40 +94,308 @@ namespace Folder_Creator_Tool_V3
                     MessageBox.Show("Connexion réussi");
                 else
                     MessageBox.Show("Connexion échouée");
+            }
 
-                 try
-                 {
-                      CurrentProjectPdmId = TopSolidHost.Pdm.GetCurrentProject();   // Récupération ID projet
-                 }
-                 catch (Exception ex)
-                 {
-                     MessageBox.Show("Echec de la récupération de l'id du projet courant" + ex.Message);
-                     return;
-                 }
+            try
+            {
+                CurrentProjectPdmId = TopSolidHost.Pdm.GetCurrentProject();   // Récupération ID projet courent
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Echec de la récupération de l'id du projet courant" + ex.Message);
+                return;
+            }
 
-                 try
-                 {
-                   CurrentProjectName = TopSolidHost.Pdm.GetName(CurrentProjectPdmId);  // Récupération Nom projet
-                   string TextBoxProjectName = CurrentProjectName;
-                 
-                    MessageBox.Show (TextBoxProjectName);
-                 }
-                 catch (Exception ex)
-                 {
-                   MessageBox.Show("Echec de la récupération de l'id du projet courant" + ex.Message);
-                 }
+            try
+            {
+                CurrentProjectName = TopSolidHost.Pdm.GetName(CurrentProjectPdmId);  // Récupération Nom projet
+                TextBoxProjectName = CurrentProjectName;
 
-                  TopSolidHost.Pdm.GetConstituents(CurrentProjectPdmId, FolderIDs);
+                textBox10.Text = TextBoxProjectName; //Affichage du nom du projet courent dans la case texte
+                textBox1.Text = TextBoxProjectName; //Affichage le N° de moule dans la case texte
 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Echec de la récupération de l'id du projet courant" + ex.Message);
+            }
 
+            try
+            {
+                CurrentDocumentId = TopSolidHost.Documents.EditedDocument;  // Récupération ID Document
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Echec de la récupération de l'id du document courant" + ex.Message);
+
+                return;
+            }
+
+            try
+            {
+                string CurrentDocumentName = TopSolidHost.Documents.GetName(CurrentDocumentId);  // Récupération du nom du document courent
+                TextCurrentDocumentName = CurrentDocumentName;
+
+                textBox9.Text = TextCurrentDocumentName; //Affichage du nom du document courent dans la case texte
+                
 
 
             }
-        }
-    }
-}
-    
+            catch (Exception ex)
+            {
+                MessageBox.Show("Echec de la récupération du nom du document courent" + ex.Message);
+            }
 
+
+            try
+            {
+                CurrentDocumentCommentaireId = TopSolidHost.Parameters.GetCommentParameter(CurrentDocumentId); ;  // Récupération du commentaire (Repère)
+                TextCurrentDocumentCommentaire = TopSolidHost.Parameters.GetTextLocalizedValue(CurrentDocumentCommentaireId);
+
+                textBox2.Text = TextCurrentDocumentCommentaire; //Affichage du commenataire (Repère) dans la case texte
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Echec de la récupération du Commentaire" + ex.Message);
+            }
+
+
+            try
+            {
+                CurrentDocumentDesignationId = TopSolidHost.Parameters.GetDescriptionParameter(CurrentDocumentId); ;  // Récupération de la désignation
+                TextCurrentDocumentDesignation = TopSolidHost.Parameters.GetTextLocalizedValue(CurrentDocumentDesignationId);
+
+                textBox3.Text = TextCurrentDocumentDesignation; //Affichage du commenataire (Repère) dans la case texte
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Echec de la récupération du Commentaire" + ex.Message);
+            }
+
+
+            textBox8.Text = "A"; //Affichage de l'indice
+            
+
+            //Recuperation du texte modifié par l'utilisateur pour nommer les dossiers.
+            TextBoxCommentaireValue = textBox2.Text; //Repere de la piece
+            TextBoxIndiceValue = textBox8.Text; //Indice de la piece
+            TextBoxDesignationValue = textBox3.Text; //Designation de la piece
+            TextBoxNomMouleValue = textBox10.Text; //Numero du moule
+
+            MessageBox.Show("dossier" + TextBoxCommentaireValue + TextBoxIndiceValue + TextBoxDesignationValue + TextBoxNomMouleValue);
+
+            NomDossierRep = textBox2.Text + " - " + textBox3.Text; //Nom du dossier repere
+
+
+            bool recommencer;
+            do
+            {
+                ConstituantFolderNames.Clear();
+                ConstituantDocumentNames.Clear(); 
+
+                ListDocumentsFoldersNames.Clear(); ;
+
+                recommencer = false; // Réinitialisez recommencer à false à chaque début de boucle
+                //Récuperation des noms de dossiers et documents
+                try
+                {
+                    TopSolidHost.Pdm.GetConstituents(CurrentProjectPdmId, out FolderIds, out DocumentsIds);
+
+                    int i; //1er index
+
+                    for (i = 0; i < FolderIds.Count; i++) //Boucle de déconte
+                    {
+                        ConstituantFolderName = TopSolidHost.Pdm.GetName(FolderIds[i]); //Obtention des noms de dossier
+
+                        ConstituantFolderNames.Add(ConstituantFolderName); // Ajoutez le nom de dossier à la liste
+                    }
+                    DossierExistant = FolderIds[i];
+
+                    int i2; //2eme index
+
+                    for (i2 = 0; i2 < DocumentsIds.Count; i++) //Boucle de déconte
+                    {
+                        ConstituantDocumentName = TopSolidHost.Pdm.GetName(DocumentsIds[i2]); //Obtention des noms de documents
+                                                                                              // Ajoutez le nom des documents à la liste
+                        ConstituantDocumentNames.Add(ConstituantDocumentName);
+                        // Retournez la liste des noms de dossiers et de documents
+                    }
+
+                    ListDocumentsFoldersNames.AddRange(ConstituantFolderNames); //Concatenation des 2 listes dans "ListDocumentsFoldersNames"
+                    ListDocumentsFoldersNames.AddRange(ConstituantDocumentNames);
+
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Echec de la récupération des nom de dossier constituant" + ex.Message);
+                }
+
+                try
+                {
+                    //recommencer = false; // Réinitialisez recommencer à false à chaque début de boucle
+                    for (int i = 0; i < ListDocumentsFoldersNames.Count; i++)
+                    {
+                        string CommentaireTxtFormat00 = textBox2.Text + "-";
+                        string CommentaireTxtFormat01 = textBox2.Text + " ";
+
+                        test00 = ListDocumentsFoldersNames[i].StartsWith(CommentaireTxtFormat00, StringComparison.OrdinalIgnoreCase);
+                        test01 = ListDocumentsFoldersNames[i].StartsWith(CommentaireTxtFormat01, StringComparison.OrdinalIgnoreCase);
+                        if (test00 ^ test01)
+                        {
+                            NomDossierExistant = ListDocumentsFoldersNames[i];
+                            if (NomDossierRep != NomDossierExistant)
+                            {
+                                DialogResult result = MessageBox.Show("Un dossier existe avec le même repère mais avec une désignation différente." + Environment.NewLine + "Merci de vérifier et de corriger avant de continuer" + Environment.NewLine + "Nom du dossier détecté = " + NomDossierExistant + Environment.NewLine + "Nom du dossier qui doit être créé : " + NomDossierRep, "Doublon potentiel", MessageBoxButtons.RetryCancel);
+
+                                if (result == DialogResult.Retry)
+                                {
+                                    recommencer = true; // Si l'utilisateur clique sur "Retry", recommencer sera true et la boucle while recommencera
+                                }
+                                else if (result == DialogResult.Cancel)
+                                {
+                                    break;
+                                }
+                            }
+                            else if (NomDossierRep == NomDossierExistant)
+                            {
+                                MessageBox.Show("le dossier " + NomDossierRep + " existe déjà. Recherche du dossier d'indice");
+
+                                IndiceFolderIds = TopSolidHost.Pdm.SearchFolderByName(DossierExistant, NomDossierExistant);
+                               
+                                for (int i3 = 0; i3 < IndiceFolderIds.Count; i3++)
+                                {
+                                    IndiceFolderName=TopSolidHost.Pdm.GetName(IndiceFolderIds[i3]);
+
+                                    if (IndiceFolderName == "Ind " + textBox8.Text);
+                                    {
+                                        MessageBox.Show("les dossiers existe deja");
+
+                                    }
+                                 
+                                    
+
+                                
+                                
+                                
+                                
+                                }
+
+
+
+
+
+                            }
+
+
+
+
+
+                        }
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Echec de la récupération des noms de dossier constituant" + ex.Message);
+                }
+            }
+            while (recommencer) ; // La boucle while recommencera si recommencer est true
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            TopSolidDesignHost.Disconnect();
+            TopSolidHost.Disconnect();
+
+            {
+                bool TSDisConnected = TopSolidDesignHost.IsConnected;
+                if (TSDisConnected == false)
+                {
+                    MessageBox.Show("Tentative de deconnexion");
+                    MessageBox.Show("Déconnexion réussi");
+                }
+                else
+                {
+                    MessageBox.Show("Déconnexion échouée. Toujours connecté");
+                }
+
+
+            }
+
+        }
+
+       static private void TopSolid_Exited(object sender, EventArgs e)
+       {
+           Application.Exit();
+       }
+
+
+    }
+            
+        
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
 
 
      
