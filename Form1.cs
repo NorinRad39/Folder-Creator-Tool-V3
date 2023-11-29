0c7e5e976525ea9ca0d5228cc659a1aa9f92b428
Florent FABBRI <Norinrad.39@gmail.com>
2023-11-30 00:43:53 +0100

    Ajout reload

--- Form1.cs
+++ Form1.cs
@@ -537,6 +537,11 @@ namespace Folder_Creator_Tool_V3
 
 
         }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
     }
 }
 
